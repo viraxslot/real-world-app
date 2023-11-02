f64cd96c9c3d2cda93820d7ade7f65f43f947412
Alex Vershinin <alexander.vershinin@quantori.com>
2023-11-02 16:01:50 +0400

    chore: added prettier, husky, lint-staged

---
+++ lint-staged.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  '*.{ts,tsx,js,jsx,html,json}': ['prettier --write'],
+  'src/**/*.{ts,tsx,js,jsx}': [
+    'eslint --fix --max-warnings=0',
+    () => 'tsc -p tsconfig.json --noEmit',
+  ],
+};
