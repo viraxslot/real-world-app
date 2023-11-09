59dc29627d82dd17c4481cc9ece2f855cffccca8
Alex Vershinin <alexander.vershinin@quantori.com>
2023-11-09 23:55:16 +0400

    chore: removed redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
 import { PageName, Paths } from './helpers/paths.ts';
 import './index.css';
 import { EditorPage } from './pages/EditorPage.tsx';
@@ -12,7 +11,7 @@ import { LoginPage } from './pages/LoginPage.tsx';
 import { ProfilePage } from './pages/ProfilePage.tsx';
 import { RegisterPage } from './pages/RegisterPage.tsx';
 import { SettingsPage } from './pages/SettingsPage.tsx';
-import { persistor, store } from './store/store.ts';
+import { store } from './store/store.ts';
 
 const router = createBrowserRouter([
   {
@@ -45,9 +44,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <RouterProvider router={router}></RouterProvider>
-      </PersistGate>
+      <RouterProvider router={router}></RouterProvider>
     </Provider>
   </React.StrictMode>,
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,41 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loginStatusReducer from './reducers/loginStatus';
 import userProfileReducer from './reducers/userProfile';
-import storage from 'redux-persist/lib/storage';
-import {
-  FLUSH,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-  REHYDRATE,
-  persistReducer,
-  persistStore,
-} from 'redux-persist';
-
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-const persistedLoginStatusReducer = persistReducer(persistConfig, loginStatusReducer);
-const persistedUserProfileReducer = persistReducer(persistConfig, userProfileReducer);
 
 export const store = configureStore({
   reducer: {
-    loginStatus: persistedLoginStatusReducer,
-    userProfile: persistedUserProfileReducer,
+    loginStatus: loginStatusReducer,
+    userProfile: userProfileReducer,
   },
-  // fixes problem with non-serializable value
-  // https://redux-toolkit.js.org/usage/usage-guide#use-with-redux-persist
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
