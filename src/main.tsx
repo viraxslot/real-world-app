f584d457afa71bd67c07f8b6f7f6067cb2843c5e
Alex Vershinin <alexander.vershinin@quantori.com>
2023-11-13 14:20:46 +0400

    chore: added imports for css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import './index.css';
 import ReactDOM from 'react-dom/client';
 import { HomePage } from './pages/HomePage.tsx';
-import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import ErrorPage from './pages/ErrorPage.tsx';
 import LoginPage from './pages/LoginPage.tsx';
