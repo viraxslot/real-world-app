c392a5f93d257e503cba12ee5f35804a912987b0
Alex Vershinin <alexander.vershinin@quantori.com>
2023-11-14 08:13:59 +0400

    test: footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom';
+import { FOOTER_LOCATORS } from './Footer.locators';
 
 export function Footer() {
   return (
     <footer>
-      <div className="container">
-        <Link to="/" className="logo-font">
+      <div className="container" data-testid={FOOTER_LOCATORS.itself}>
+        <Link to="/" className="logo-font" data-testid={FOOTER_LOCATORS.homePage}>
           conduit
         </Link>
         <span className="attribution">
-          An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code
-          &amp; design licensed under MIT.
+          An interactive learning project from{' '}
+          <a href="https://thinkster.io" data-testid={FOOTER_LOCATORS.thinkster}>
+            Thinkster
+          </a>
+          . Code &amp; design licensed under MIT.
         </span>
       </div>
     </footer>
