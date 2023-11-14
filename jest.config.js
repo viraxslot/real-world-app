f101d0c219194ef6b38bb6afe17660c8c61b01b5
Alex Vershinin <alexander.vershinin@quantori.com>
2023-11-14 15:50:54 +0400

    test: register page

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,29 @@ export default {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!<rootDir>/src/config/**'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        diagnostics: {
+          ignoreCodes: [1343],
+        },
+        astTransformers: {
+          before: [
+            {
+              path: 'ts-jest-mock-import-meta',
+              options: {
+                metaObjectReplacement: {
+                  env: {
+                    VITE_API_URL: '',
+                    VITE_ENABLE_DEV_MODE: 'true',
+                  },
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
+  },
 };
